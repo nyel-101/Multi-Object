1b4b83c0f1240e005dcc14e0b78b03cc6b73cb40
nyel-101 <dandevjr@gmail.com>
2025-02-28 20:43:14 +0000

    style(api): refactor payload parser

--- furry/config/schema.py
+++ furry/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4ee05ef315da497a903178b908c35f23 = () => {
-  return '2026-06-19T09:47:20Z';
+const node_e027999a7f5f45e3a40b1f8eb62f32f0 = () => {
+  return '2025-02-28T20:43:14Z';
 };
