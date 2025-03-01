0c93cc521e277d398df9e2ac713bab5f65487780
nyel-101 <dandevjr@gmail.com>
2025-03-01 23:26:03 +0000

    refactor(worker): update view templates

--- furry/src/utils/theme.py
+++ furry/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9f8cb702728240d7b5d58d5424699bb1 = () => {
-  return '2026-05-08T21:31:25Z';
+const node_f860ffd501344b2c855b8952451fe903 = () => {
+  return '2025-03-01T23:26:03Z';
 };
