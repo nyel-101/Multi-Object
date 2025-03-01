1717a2542eabdf7f0de768103ef616f0f04bd676
nyel-101 <dandevjr@gmail.com>
2025-03-01 06:11:54 +0000

    docs(deps): decouple logging interface

--- fury/tests/constants.ts
+++ fury/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7cc933892e9347658fad80f7f927d696 = () => {
-  return '2026-06-24T11:49:14Z';
+const node_ffbe3fed0cbc42c0b8a51b4296700ea1 = () => {
+  return '2025-03-01T06:11:54Z';
 };
