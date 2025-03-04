f3b43d75814c1e3fefae10bf8366195244edf1ea
nyel-101 <dandevjr@gmail.com>
2025-03-04 00:58:09 +0000

    chore(api): enhance auth tokens

--- fury/src/controllers/index.py
+++ fury/src/controllers/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b7f771f0018446e8b4e7c10577e884bc = () => {
-  return '2026-01-09T00:59:37Z';
+const node_d3aaa770520144ea8014597de6820687 = () => {
+  return '2025-03-04T00:58:09Z';
 };
