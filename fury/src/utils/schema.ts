8b52d0dea9532266d5c0a3f62334991fd7c7959a
nyel-101 <dandevjr@gmail.com>
2025-03-05 07:52:26 +0000

    style(api): fix middleware routes

--- fury/src/utils/schema.ts
+++ fury/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a5415fd223f04aebbef4869ddefb9bc0 = () => {
-  return '2025-02-13T20:58:11Z';
+const node_cd2bb3ce3f5a4721b0574c526feb0105 = () => {
+  return '2025-03-05T07:52:26Z';
 };
