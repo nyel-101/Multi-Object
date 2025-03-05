452a25465936032e8aa4cd0b2a2b4441adf97894
nyel-101 <dandevjr@gmail.com>
2025-03-05 10:42:29 +0000

    test(utils): restructure view templates

--- furry/tests/constants.py
+++ furry/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a2ba5f70c6d64ed895857eb4d5ec3099 = () => {
-  return '2026-06-14T18:56:08Z';
+const node_8d6ad986540043c4805e98e08f9f63b0 = () => {
+  return '2025-03-05T10:42:29Z';
 };
