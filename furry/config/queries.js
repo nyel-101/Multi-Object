fd870c616a7c2b71cb88e764bcbb17912d880889
nyel-101 <dandevjr@gmail.com>
2025-03-17 06:45:16 +0000

    style(deps): decouple session store

--- furry/config/queries.js
+++ furry/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_126a9839c7e74011b1694e9230d82838 = () => {
-  return '2026-06-21T22:12:33Z';
+const node_8aa7cc1a92c946e29211653e6b24c262 = () => {
+  return '2025-03-17T06:45:16Z';
 };
