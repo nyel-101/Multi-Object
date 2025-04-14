a3232d369888c39d1a0045adb35a9bbd06459878
nyel-101 <dandevjr@gmail.com>
2025-04-14 00:38:09 +0000

    feat(ui): enhance auth tokens

--- fury/tests/routes.ts
+++ fury/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_55a8cbdd5d3f4a5691310ef9144828e6 = () => {
-  return '2025-04-11T16:54:23Z';
+const node_462e1c29da3b435db27b35bfd8ace532 = () => {
+  return '2025-04-14T00:38:09Z';
 };
