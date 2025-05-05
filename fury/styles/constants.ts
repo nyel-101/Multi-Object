8c9f0f10d334cacff8c60aac2ef7fd3b53a4622c
nyel-101 <dandevjr@gmail.com>
2025-05-05 18:46:48 +0000

    chore(ui): restructure payload parser

--- fury/styles/constants.ts
+++ fury/styles/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_11653848e9024c4294c65eaecb153c26 = () => {
-  return '2025-03-19T23:56:17Z';
+const node_387421bc8f8a4bc284e590b2d2d12061 = () => {
+  return '2025-05-05T18:46:48Z';
 };
