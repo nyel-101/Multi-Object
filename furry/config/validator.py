f4e28b9c54740c1c61dadc54b9a063f4df51a58f
nyel-101 <dandevjr@gmail.com>
2025-05-09 08:27:53 +0000

    style(ui): decouple middleware routes

--- furry/config/validator.py
+++ furry/config/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eb0e8400d9cd490f8cbc6fe3783783ea = () => {
-  return '2025-03-19T18:11:54Z';
+const node_3b9b31b151664da78db99775f4278acb = () => {
+  return '2025-05-09T08:27:53Z';
 };
