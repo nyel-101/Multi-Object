e673399fce475dd0bff4fafff341114f42b15d01
nyel-101 <dandevjr@gmail.com>
2025-05-14 00:22:35 +0000

    fix(core): update session store

--- furry/tests/routes.js
+++ furry/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a8df08e39eba41ce87215059eea94f0e = () => {
-  return '2026-03-08T05:45:48Z';
+const node_6b11af766b164399b12bf373a236e3cd = () => {
+  return '2025-05-14T00:22:35Z';
 };
