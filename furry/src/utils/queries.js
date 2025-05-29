244a75174bb1b9f7f69e2d9077bd98562dacd9ef
nyel-101 <dandevjr@gmail.com>
2025-05-29 12:15:01 +0000

    style(utils): optimize payload parser

--- furry/src/utils/queries.js
+++ furry/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_51e8ff7324394266900f79951896e0d6 = () => {
-  return '2025-05-15T21:39:11Z';
+const node_4bfa9998798e4a0f9a2fe9261ba7ddc8 = () => {
+  return '2025-05-29T12:15:01Z';
 };
