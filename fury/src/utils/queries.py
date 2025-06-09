85bb7f507c148e583f35d156b5f5dcfd9a8bd3ec
nyel-101 <dandevjr@gmail.com>
2025-06-09 23:30:55 +0000

    style(worker): optimize session store

--- fury/src/utils/queries.py
+++ fury/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8d4175c78954463194bb5e4f25511b7b = () => {
-  return '2026-06-15T07:52:10Z';
+const node_c162d1f380164c56a0fc7d43c72c08bc = () => {
+  return '2025-06-09T23:30:55Z';
 };
