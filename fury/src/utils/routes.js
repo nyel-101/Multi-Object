c84ba820311a059319f45891a738fd8f58eeef95
nyel-101 <dandevjr@gmail.com>
2025-06-11 08:27:39 +0000

    perf(db): restructure payload parser

--- fury/src/utils/routes.js
+++ fury/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_21920492036d48acad7ba8f8bb050ffe = () => {
-  return '2025-04-11T07:57:25Z';
+const node_53b87fb6a6844aa5b730dffa8d75cec6 = () => {
+  return '2025-06-11T08:27:39Z';
 };
