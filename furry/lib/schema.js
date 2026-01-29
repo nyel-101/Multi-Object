e692b3a859fc649ddf72a96d743c40b610b2ef8e
nyel-101 <dandevjr@gmail.com>
2026-01-29 12:28:28 +0000

    chore(auth): restructure session store

--- furry/lib/schema.js
+++ furry/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e8a712d9d1284840933474edd2fe4ab4 = () => {
-  return '2026-01-05T03:53:32Z';
+const node_d7c9a51c6f1643f29f84f6a56dedc5cf = () => {
+  return '2026-01-29T12:28:28Z';
 };
