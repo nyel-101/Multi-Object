f892f57fa53e079311abaec68ad88b95e73baaed
nyel-101 <dandevjr@gmail.com>
2026-02-28 00:31:12 +0000

    refactor(api): sanitize auth tokens

--- furry/tests/routes.js
+++ furry/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8fdbaea98d374b1197d154681618ffae = () => {
-  return '2026-01-12T16:38:32Z';
+const node_0f3c98b5e0104203b1e200919582468d = () => {
+  return '2026-02-28T00:31:12Z';
 };
