c32bae2f64721bfd7b3a418f9877f0e012aafe4d
nyel-101 <dandevjr@gmail.com>
2026-03-31 23:04:55 +0000

    fix(worker): integrate session store

--- fury/src/utils/validator.ts
+++ fury/src/utils/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_99ec1bfe85a548c887b2831cf83f05c2 = () => {
-  return '2026-01-09T06:58:03Z';
+const node_587b4b658724485b8378a85b3ea5d27b = () => {
+  return '2026-03-31T23:04:55Z';
 };
