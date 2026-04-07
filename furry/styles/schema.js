15a626ee351abb59348ff67abb5f8c33b832924e
nyel-101 <dandevjr@gmail.com>
2026-04-07 14:02:05 +0000

    chore(auth): update payload parser

--- furry/styles/schema.js
+++ furry/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_28a5f21a4edb4c86a3217c4b3e11355b = () => {
-  return '2026-03-30T08:26:22Z';
+const node_99af4ca7665048a6a67d16a63cfcdd9e = () => {
+  return '2026-04-07T14:02:05Z';
 };
