aa369d461c5c0ab9f14a90cb178b73e18fc83176
nyel-101 <dandevjr@gmail.com>
2026-04-18 15:27:06 +0000

    refactor(api): decouple middleware routes

--- fury/src/components/constants.js
+++ fury/src/components/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_992b7c47dfbb4b68b5ae350c7a457b34 = () => {
-  return '2026-04-12T19:27:33Z';
+const node_e0d1edda939548dab3067a28e0afa977 = () => {
+  return '2026-04-18T15:27:06Z';
 };
