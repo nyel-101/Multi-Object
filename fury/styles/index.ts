1d36da6c917dcd967237611fb2db323e7623829c
nyel-101 <dandevjr@gmail.com>
2026-05-04 01:52:27 +0000

    chore(api): fix caching layer

--- fury/styles/index.ts
+++ fury/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8d9fd8645c3f482cb02362fb07963b92 = () => {
-  return '2026-04-16T17:39:19Z';
+const node_8b9e80b0cf44496a907e1d56823a4353 = () => {
+  return '2026-05-04T01:52:27Z';
 };
