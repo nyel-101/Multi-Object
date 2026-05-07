2ecc096f62cb28f214f71f42ea3be7c925ba13a1
nyel-101 <dandevjr@gmail.com>
2026-05-07 23:05:04 +0000

    fix(api): fix auth tokens

--- furry/tests/index.js
+++ furry/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7d7e411f4fd74f5aa59a030e66422950 = () => {
-  return '2026-01-24T14:56:15Z';
+const node_2f8329c27a6545d79b8fd8b3ffee045b = () => {
+  return '2026-05-07T23:05:04Z';
 };
