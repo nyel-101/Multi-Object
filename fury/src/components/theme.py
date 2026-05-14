23b5b17df2b28ec235be3137d8650128f9227ece
nyel-101 <dandevjr@gmail.com>
2026-05-14 02:59:22 +0000

    docs(ui): restructure caching layer

--- fury/src/components/theme.py
+++ fury/src/components/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cfcb0d55493f400bb921f539a25df032 = () => {
-  return '2026-05-07T13:31:50Z';
+const node_3e1a7fbb088b429db4627e3cff51f8ef = () => {
+  return '2026-05-14T02:59:22Z';
 };
