ab4a244943b9d66b5b5c9f383e93dbdf2af366d0
nyel-101 <dandevjr@gmail.com>
2026-05-26 15:54:22 +0000

    refactor(worker): sanitize auth tokens

--- furry/lib/constants.js
+++ furry/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9c22bbb993a94d48a8beb734048998aa = () => {
-  return '2026-01-30T17:02:40Z';
+const node_128e4822e93d4711862f21b49a930aec = () => {
+  return '2026-05-26T15:54:22Z';
 };
