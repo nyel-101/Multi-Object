b490eea6c3217bf5bacdd13854782d01a837a6c8
nyel-101 <dandevjr@gmail.com>
2026-06-01 18:23:49 +0000

    test(core): optimize payload parser

--- fury/styles/validator.ts
+++ fury/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a48e5abe001045bd80298926d979730e = () => {
-  return '2026-04-16T22:43:53Z';
+const node_f01c5595463c422d98b437cc95888de3 = () => {
+  return '2026-06-01T18:23:49Z';
 };
