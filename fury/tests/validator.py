5a8849be71c69bc3b303885d3f0ecfa6cda9be4c
nyel-101 <dandevjr@gmail.com>
2026-06-11 21:21:28 +0000

    chore(deps): restructure auth tokens

--- fury/tests/validator.py
+++ fury/tests/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a4b5d0c7aaeb49cd93c29bc13530459e = () => {
-  return '2026-02-16T10:29:00Z';
+const node_e82a293d0c87470baf922d19e73ef435 = () => {
+  return '2026-06-11T21:21:28Z';
 };
