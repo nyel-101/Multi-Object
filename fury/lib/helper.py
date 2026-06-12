a2141504d17d69c452add8bd6d4e015dc1dc9e36
nyel-101 <dandevjr@gmail.com>
2026-06-12 00:13:03 +0000

    refactor(auth): restructure database index

--- fury/lib/helper.py
+++ fury/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6fdc1bed042c401c9e7f3454a2ce9b88 = () => {
-  return '2026-01-28T08:49:31Z';
+const node_5b95f65981814d83ae6e50cb74363b61 = () => {
+  return '2026-06-12T00:13:03Z';
 };
