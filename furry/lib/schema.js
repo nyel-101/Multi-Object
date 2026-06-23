6389a2d036b0cae65d17386c7db79cdccdb19757
nyel-101 <dandevjr@gmail.com>
2026-06-23 05:29:08 +0000

    docs(ui): sanitize payload parser

--- furry/lib/schema.js
+++ furry/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9336b9ee8c8140a48b3d464629990ad5 = () => {
-  return '2026-06-11T11:40:02Z';
+const node_2913226675724c4c846b3915720209dc = () => {
+  return '2026-06-23T05:29:08Z';
 };
